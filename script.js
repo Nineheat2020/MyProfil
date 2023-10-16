00773b4e989499f46eb1dc30f7c4d2ecbf4ff0d9
call me Fahrezi <104294039+Nineheat2020@users.noreply.github.com>
2023-10-16 17:47:07 +0700

    Create script.js
    menambahkan file script.js

---
+++ script.js
@@ -0,0 +1,54 @@
+function handleGetFormData() {
+  const name = document.getElementById("name").value;
+  const city = document.getElementById("city").value;
+  const email = document.getElementById("email").value;
+  const zipCode = document.getElementById("zip-code").value;
+  const status = document.getElementById("status").checked;
+
+  return {
+    name: name,
+    city: city,
+    email: email,
+    zipCode: zipCode,
+    status: status
+  };
+}
+
+function isNumber(zipCode) {
+  if (zipCode && !isNaN(zipCode)) {
+    return true
+  } else {
+    return false
+  }
+}
+
+function checkboxIsChecked() {
+  var checkbox = document.getElementById("status");
+  if (checkbox) {
+    return checkbox.checked;
+  } else {
+    return false;
+  }
+}
+
+function validateFormData(props) {
+  if (props.name !== "" && props.city !== "" && props.email !== "" && isNumber(props.zipCode) && checkboxIsChecked(props.status)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function submit() {
+  if (!validateFormData(handleGetFormData())) {
+    return (document.getElementById('warning').innerText = 'Periksa form anda sekali lagi');
+  } else {
+    return (document.getElementById('warning').innerText = '');
+  }
+}
+
+document.getElementById("submit-form").addEventListener("click", function(event) {
+  submit();
+  event.preventDefault();
+  resetForm()
+});
